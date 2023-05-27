8cbb11b902aec6e689f4f0d745191d5468ae181f
Šimon Zasadil <zasadil@damidev.com>
2023-05-27 19:50:43 +0200

    fix: target _blank missing underscore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         Welcome!
       </h1>
       <p>This is my <i>sample project</i> about blog posts. View my code at <Link className='blue-link' href={'https://github.com/simzas/blog-sample'}>GitHub</Link>.</p>
-      <p>It uses <Link className="blue-link" href={'https://jsonplaceholder.typicode.com/'} target='blank'>JSON Placeholder API</Link> to get data.</p>
+      <p>It uses <Link className="blue-link" href={'https://jsonplaceholder.typicode.com/'} target='_blank'>JSON Placeholder API</Link> to get data.</p>
       <Link href={'/posts'} className='self-center rounded-lg border-2 border-slate-200 p-4 my-8 hover:bg-slate-900'>Show me</Link>
     </section>
   )
