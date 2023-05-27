42411284b32ee640e897d1cce635c6e077fe10d9
Šimon Zasadil <zasadil@damidev.com>
2023-05-28 01:50:41 +0200

    fix: layout responsive with footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-950 text-slate-200`}>
+      <body className={`${inter.className} bg-slate-950 text-slate-200 min-h-screen flex flex-col`}>
         <Navbar />
-        <div className='flex justify-center'>
+        <div className='flex justify-center flex-1'>
           {children}
         </div>
         <Footer />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
         <nav className='
         sm:sticky top-0 flex flex-col 
         justify-between align-center px-4 mb-2 py-4 
-        sm:flex-row sm:py-6 sm:mb-6
+        sm:flex-row sm:py-6 
         text-xl border-b-[1px] border-neutral-800 bg-[#060815]'>
             <ul className='flex flex-col sm:flex-row align-center'>
                 <Link href={'/'} className='px-4 py-4 sm:py-0 sm:hover:underline hover:bg-slate-800 sm:hover:bg-transparent hover:cursor-pointer'>Home</Link>
