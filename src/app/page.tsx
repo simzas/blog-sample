8100ced75cf459d91ce7cd88081608024e68b12e
Šimon Zasadil <zasadil@damidev.com>
2023-05-27 19:47:59 +0200

    feat: welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <h1 >
+    <section className='flex flex-col min-h-[50vh]'>
+      <h1 className='text-5xl pb-2'>
         Welcome!
       </h1>
-      <p>This is my sample project of blog posts.</p>
-      <p>It uses <Link href={'https://jsonplaceholder.typicode.com/'} target='blank'>JSON Placeholder API</Link> to get data.</p>
-      <div>Go to <Link href={"/posts"} className='hover:underline'>posts</Link></div>
-    </>
+      <p>This is my <i>sample project</i> about blog posts. View my code at <Link className='blue-link' href={'https://github.com/simzas/blog-sample'}>GitHub</Link>.</p>
+      <p>It uses <Link className="blue-link" href={'https://jsonplaceholder.typicode.com/'} target='blank'>JSON Placeholder API</Link> to get data.</p>
+      <Link href={'/posts'} className='self-center rounded-lg border-2 border-slate-200 p-4 my-8 hover:bg-slate-900'>Show me</Link>
+    </section>
   )
 }
