217c232e3674c42dec69b43532c441128fe0e836
Šimon Zasadil <zasadil@damidev.com>
2023-05-28 01:08:05 +0200

    feat: sticky header + background color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 
 const Navbar: React.FC = () => {
     return (
-        <nav className='flex 
-        flex-col 
-        sm:flex-row sm:py-6 sm:mb-6
+        <nav className='
+        sm:sticky top-0 flex flex-col 
         justify-between align-center px-4 mb-2 py-4 
-        border-b-2 border-neutral-800 text-xl'>
+        sm:flex-row sm:py-6 sm:mb-6
+        text-xl border-b-2 border-neutral-800 bg-[#060815]'>
             <ul className='flex flex-col sm:flex-row align-center'>
                 <Link href={'/'} className='px-4 py-4 sm:py-0 sm:hover:underline hover:bg-slate-800 sm:hover:bg-transparent hover:cursor-pointer'>Home</Link>
                 <Link href={'/posts'} className='px-4 py-4 sm:py-0 sm:hover:underline hover:bg-slate-800 sm:hover:bg-transparent hover:cursor-pointer'>Posts</Link>
