b14431966793a34976aa0625c7226f5593d62786
Šimon Zasadil <zasadil@damidev.com>
2023-05-27 19:42:14 +0200

    feat + fix: navbar responsiveness + design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,17 +10,11 @@ const Navbar: React.FC = () => {
         justify-between align-center px-4 mb-2 py-4 
         border-b-2 border-neutral-800 text-xl'>
             <ul className='flex flex-col sm:flex-row align-center'>
-                <li className='px-4 py-4 sm:py-0 hover:underline'>
-                    <Link href={'/'}>Home</Link>
-                </li>
-                <li className='px-4 py-4 sm:py-0 hover:underline'>
-                    <Link href={'/posts'}>Posts</Link>
-                </li>
+                <Link href={'/'} className='px-4 py-4 sm:py-0 sm:hover:underline hover:bg-slate-800 sm:hover:bg-transparent hover:cursor-pointer'>Home</Link>
+                <Link href={'/posts'} className='px-4 py-4 sm:py-0 sm:hover:underline hover:bg-slate-800 sm:hover:bg-transparent hover:cursor-pointer'>Posts</Link>
             </ul>
             <ul className='flex flex-col sm:flex-row align-center'>
-                <li className='px-4 py-4 sm:py-0 hover:underline'>
-                    <Link href={'/about'}>About</Link>
-                </li>
+                <Link href={'/about'} className='px-4 py-4 sm:py-0 sm:hover:underline hover:bg-slate-800 sm:hover:bg-transparent hover:cursor-pointer'>About</Link>
             </ul>
         </nav>
     )
