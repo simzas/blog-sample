7357bcef6965d6b64f569d0dc4b00765002d414a
Šimon Zasadil <zasadil@damidev.com>
2023-05-28 03:52:06 +0200

    feat: profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+
+    return (
+        <section className="flex flex-col">
+            <p>Profile page</p>
+            <p>https://jsonplaceholder.typicode.com/users/2</p>
+        </section>
+    )
+}
