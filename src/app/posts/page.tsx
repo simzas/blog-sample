ff54dd6f086db4704aceebc31eb555f00ef67d74
Šimon Zasadil <zasadil@damidev.com>
2023-05-28 04:00:17 +0200

    fix: build error on singe ( ' ) quotes inside text

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -135,7 +135,7 @@ export default function Post() {
                             <p className="px-4 py-2 text-slate-400 text-sm">Author</p>
                             <div className="border-b-[1px] border-slate-800 my-1"></div>
                             <div className="p-4">
-                                <span>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</span>
+                                <span>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ´Content here, content here´, making it look like readable English.</span>
                             </div>
                         </div>
                     </div>
@@ -146,7 +146,7 @@ export default function Post() {
                             <p className="px-4 py-2 text-slate-400 text-sm">Author</p>
                             <div className="border-b-[1px] border-slate-800 my-1"></div>
                             <div className="p-4">
-                                <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
+                                <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>
                             </div>
                         </div>
                     </div>
