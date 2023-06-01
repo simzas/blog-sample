b070039c6ec4620f21d8ee4423382f112eb59d20
Šimon Zasadil <zasadil@damidev.com>
2023-06-02 00:12:04 +0200

    loading componenets to Array of 10

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -5,12 +5,9 @@ export default function Loading() {
     return (
         <section className="p-4 md:px-12 md:py-8">
             <h3 className="text-3xl mb-4">Newest posts</h3>
-            <LoadingPostComponent key='1' />
-            <LoadingPostComponent key='2' />
-            <LoadingPostComponent key='3' />
-            <LoadingPostComponent key='4' />
-            <LoadingPostComponent key='5' />
-            <LoadingPostComponent key='6' />
+            {[...Array(10).keys()].map((i) => (
+                <LoadingPostComponent key={i} />
+            ))}
         </section>
     )
 }
