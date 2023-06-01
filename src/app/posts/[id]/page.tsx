91740fe9bfbe264fafb094f8f062e643a89795be
Šimon Zasadil <zasadil@damidev.com>
2023-06-01 17:03:36 +0200

    feat: /posts/[id]

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function PostDetail() {
+
+    return (
+        <div className="flex flex-col">
+            <p>Post detail page....TODO</p>
+            <p>https://jsonplaceholder.typicode.com/posts/2</p>
+        </div>
+    )
+}
